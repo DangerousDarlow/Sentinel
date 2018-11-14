8707621daad5a9ac7ec8ce54e3c89b4e9f471e3a
Nick Darlow <dangerous.darlow@gmail.com>
2018-11-14 22:54:56 +0000

    Add nexmo application config

---
+++ src/main/kotlin/com/nidicate/sentinel/NexmoConfig.kt
@@ -0,0 +1,11 @@
+package com.nidicate.sentinel
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+
+@Component
+@ConfigurationProperties("nexmo")
+class NexmoConfig {
+    lateinit var applicationId: String
+    lateinit var privateKey: String
+}
